31b62199e2fe1b0949d6bd0afa572c672db14e89
Mafin116 <uv00@list.ru>
2017-11-05 21:29:20 +0300

    BE05. Your First .gitignore for Unreal

---
+++ BildingEscape/Source/BildingEscape/BildingEscape.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BildingEscape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BildingEscape, "BildingEscape" );
