b5410e2792ecd519d47bca8abf6835d1e21c3520
Mafin <uv00@list.ru>
2017-11-26 15:10:41 +0300

    78. 1111

--- BildingEscape/Source/BildingEscape/OpenDoor.cpp
+++ BildingEscape/Source/BildingEscape/OpenDoor.cpp
@@ -19,15 +19,18 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// Find the owning Actor
-	AActor* Owner = GetOwner();
-	
-	// Create a rotator
-	FRotator NewRotation = FRotator(0.f, -60.f, 0.f);
-	
-	// Set the door rotation
-	Owner->SetActorRotation(NewRotation);
-	
+	Owner = GetOwner();
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+void UOpenDoor::OpenDoor()
+{
+	Owner->SetActorRotation(FRotator(0.f, OpenAngel, 0.f));
+}
+
+void UOpenDoor::CloseDoor()
+{
+	Owner->SetActorRotation(FRotator(0.f, 0.f, 0.f));
 }
 
 
@@ -36,6 +39,17 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
-}
+	// Poll the Trigger Volume
+	// If the ActorThatOpens is in the volume
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+	}
 
+	// Check if it's time to close the door
+	if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay)
+	{
+		CloseDoor();
+	}
+}
