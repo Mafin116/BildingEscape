57194d923a7a698f6a7de3f54586e6aa9ad20b66
Mafin116 <uv00@list.ru>
2017-11-26 14:30:46 +0300

    78. Using DrawDebugLine

--- BildingEscape/Source/BildingEscape/Grabber.cpp
+++ BildingEscape/Source/BildingEscape/Grabber.cpp
@@ -38,10 +38,28 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		OUT PlayerViewPointRotation
 	);
 
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"),
+	/*UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"),
 		*PlayerViewPointLocation.ToString(),
 		*PlayerViewPointRotation.ToString()
-	)
+	)*/
 
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	// Draw a red trace in the world to visual
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);
+
+	// Ray-cast out to reach distace
+
+	// See what we hit
 }
 
